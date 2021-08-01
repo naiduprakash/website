e0a542e10390e1675f6fd38473de6b3859bf0d14
naiduprakash <prakash.naidu20101995@gmail.com>
2021-08-01 13:25:21 +0000

    Updates

---
+++ static/js/main.3c6ff803.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpwebsite=this.webpackJsonpwebsite||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),s=n.n(c),i=n(3),r=n.n(i),a=(n(8),n(9),n(0));var o=function(){return Object(a.jsx)("div",{className:"App",children:"Prakash Naidu (Fullstack web developer)"})};r.a.render(Object(a.jsx)(s.a.StrictMode,{children:Object(a.jsx)(o,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.3c6ff803.chunk.js.map
